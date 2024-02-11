b28da07b0ac931b027e9e24c8f6936423fcaccf0
LeaBFLL <144031817+LeaBFLL@users.noreply.github.com>
2024-02-11 16:19:39 +0100

    Update random_maze.py
    elements de correction

--- random_maze.py
+++ random_maze.py
@@ -1,1 +1,24 @@
+file = open("random-maze.py", mode="rt", encoding="ascii")
+random_maze_repr = file.read()
+file.close()
+
+random_maze = set()
+for line in random_maze_repr.splitlines():
+    line = line.strip()
+    if line not in ("{", "}", ""):
+        core = line[1:-2]
+        x, y = core.split(",")
+        x, y = int(x.strip()), int(y.strip())
+        random_maze.add((x, y))
+
+random_maze = eval(maze_repr)
+
+def load_maze(filename):
+    file = open(filename, mode="rt", encoding="ascii")
+    maze_repr = file.read()
+    file.close()
+    maze = eval(maze_repr)
+    return maze
+
+random_maze = load_maze("random-maze.py")
 
