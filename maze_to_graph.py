e3bc8f6c7c958761b0ecb0f5ba2e0cbb1bb33ce0
LeaBFLL <144031817+LeaBFLL@users.noreply.github.com>
2024-02-11 16:55:30 +0100

    Create maze_to_graph.py
    elements de correction

---
+++ maze_to_graph.py
@@ -0,0 +1,15 @@
+UP, RIGHT, DOWN, LEFT = (0, -1), (1, 0), (0, 1), (-1, 0) 
+
+def maze_to_graph(maze):
+    vertices = set(maze)
+    edges = set()
+    weights = {}
+    for vertex in vertices:
+        x, y = vertex
+        for (dx, dy) in [UP, RIGHT, DOWN, LEFT]:
+            neighbor = (x + dx, y + dy)
+            if neighbor in vertices:
+                edge = (vertex, neighbor)
+                edges.add(edge)
+                weights[edge] = 1
+    return (vertices, edges, weights)
